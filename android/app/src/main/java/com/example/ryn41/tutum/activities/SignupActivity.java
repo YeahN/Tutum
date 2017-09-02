dda9597f9667a8156cb24f9ad9d8bd67c067fd59
godjakoo <godjakoo@onface.net>
2017-09-02 16:53:01 +0900

    add signup asynctask

--- android/app/src/main/java/com/example/ryn41/tutum/activities/SignupActivity.java
+++ android/app/src/main/java/com/example/ryn41/tutum/activities/SignupActivity.java
@@ -1,11 +1,16 @@
 package com.example.ryn41.tutum.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.ryn41.tutum.R;
 
@@ -20,71 +25,82 @@ import java.net.URL;
  */
 
 public class SignupActivity extends Activity {
+    private String idstr = "";
+    private String pwstr = "";
+
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_signup);
     }
 
     @Override
-    public void onResume(){
+    public void onResume() {
         super.onResume();
         makeView();
     }
 
     @Override
-    public void onPause(){
+    public void onPause() {
         super.onPause();
     }
 
-    private void makeView(){
-
+    private void makeView() {
+        ((Button) findViewById(R.id.activity_signup_signup_button)).setOnClickListener(click);
     }
 
-    View.OnClickListener click= new View.OnClickListener(){
+    View.OnClickListener click = new View.OnClickListener() {
         @Override
-        public void onClick(View v){
-            int id= v.getId();
+        public void onClick(View v) {
+            int id = v.getId();
+
+            if (id == R.id.activity_signup_signup_button) {
+                idstr = ((EditText) findViewById(R.id.activity_signup_id_edittext)).getText().toString();
+                pwstr = ((EditText) findViewById(R.id.activity_signup_password_edittext)).getText().toString();
+
+                if (!idstr.isEmpty() && !pwstr.isEmpty()) {
+                    request();
+                } else {
+                    Toast.makeText(getApplicationContext(), "입력하세요", Toast.LENGTH_SHORT).show();
+                }
+            }
         }
     };
 
+    private void request() {
+        (new SignupAsync()).execute();
+    }
+
     private class SignupAsync extends AsyncTask<Void, Void, Void> {
         @Override
-        protected Void doInBackground(Void... params){
+        protected Void doInBackground(Void... params) {
             try {
-                URL url= new URL("http://asokoya.jp/wp-json/jwt-auth/v1/token");
-                HttpURLConnection conn= (HttpURLConnection)url.openConnection();
+                String str = "http://13.59.135.92/register.php?id=" + idstr + "&pw=" + pwstr;
+                URL url = new URL(str);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
                 conn.setRequestMethod("POST");
                 conn.setRequestProperty("Accept", "application/json");
                 conn.setRequestProperty("Content-Type", "application/json");
                 conn.setDoInput(true);
                 conn.setDoOutput(true);
 
-                OutputStream os= conn.getOutputStream();
+                OutputStream os = conn.getOutputStream();
 //                os.write(postData.toString().getBytes("utf-8"));
                 os.flush();
                 conn.connect();
 
-                BufferedReader rd  = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
                 StringBuilder sb = new StringBuilder();
                 String line;
-                while((line= rd.readLine()) != null){
+                while ((line = rd.readLine()) != null) {
                     sb.append(line);
                 }
-                line= sb.toString();
-//                Log.e("onface player", "onface_player login info : " + line);
-//                JSONObject obj= new JSONObject(line);
-//                if(obj.getString("token") != null && obj.getString("token").length() > 10){
-//
-//                    reqLogin(obj.getString("token"));
-//                } else {
-//                    Toast.makeText(getApplicationContext(), "There\'s no matched user!", Toast.LENGTH_SHORT).show();
-//                }
-//                reqLogin();
-            } catch (Exception ex){
+                line = sb.toString();
+
+                Log.e("tutum", line);
+                Toast.makeText(getApplicationContext(), "가입이 완료되었습니다.", Toast.LENGTH_SHORT).show();
+                finish();
+            } catch (Exception ex) {
                 ex.printStackTrace();
             }
             return null;
